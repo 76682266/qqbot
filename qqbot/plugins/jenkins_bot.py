e01da2c5b1040cc9ee8322bf36066ea30690b1bc
seven <seven@fanruan.com>
2017-07-19 17:48:57 +0800

    git修改测试

---
+++ qqbot/plugins/jenkins_bot.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def jenkins_bot(content):
+	#用来处理jenkins的命令
+	pass
--- qqbot/plugins/sample.py
+++ qqbot/plugins/sample.py
@@ -9,3 +9,8 @@ def onQQMessage(bot, contact, member, content):
     elif content == '-stop':
         bot.SendTo(contact, 'QQ机器人已关闭')
         bot.Stop()
+	elif content == u'-whoareu':
+		bot.SendTo(contact,'这个问题只能你自己去想了~')
+	elif '-' in content:
+		bot.SendTo(contact,'发现未知命令体,即将反馈给管理员')
+		bot.SendTo(3004188173, content, resendOn1202=True)
